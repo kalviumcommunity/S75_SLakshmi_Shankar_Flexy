343b82e94a469c71c306166bcfb7dde57bbf62fd
unknown <lakshmi7708671565@gmail.com>
2025-06-24 17:39:54 +0530

    Frontend components created

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
+// const http = require("http");
+// const { Server } = require("socket.io");
 require("dotenv").config();
 
 const userRoute = require("./Routes/Client-routes");
 const expertRoute = require("./Routes/Expert-routes");
 
 const app = express();
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
 const allowedOrigins = [
   "http://localhost:5173" 
@@ -26,34 +26,34 @@ app.use("/api", userRoute);
 app.use("/api", expertRoute);
 
 
-const io = new Server(server, {
-  cors: {
-    origin: allowedOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  }
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: allowedOrigins,
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   }
+// });
 
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+// io.on("connection", (socket) => {
+//   console.log("User connected:", socket.id);
 
-  socket.on("send_message", (data) => {
-    console.log("Message received:", data);
-    socket.broadcast.emit("receive_message", data);
-  });
+//   socket.on("send_message", (data) => {
+//     console.log("Message received:", data);
+//     socket.broadcast.emit("receive_message", data);
+//   });
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected:", socket.id);
+//   });
+// });
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI)
   .then(() => {
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
     console.log("MongoDB connected");
